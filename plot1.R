ff6e73075b4f2b57ac9ad285479fa4d1cd5b5997
Luis Hernandez <45669391+luisrh01@users.noreply.github.com>
2019-11-02 12:59:21 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,19 +1,27 @@
-# First you will need to fork and clone the following GitHub repository: https://github.com/rdpeng/ExData_Plotting1
-# 
-# For each plot you should
-# 
-# Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
-# Name each of the plot files as \color{red}{\verb|plot1.png|}plot1.png, \color{red}{\verb|plot2.png|}plot2.png, etc.
-# Create a separate R code file (\color{red}{\verb|plot1.R|}plot1.R, \color{red}{\verb|plot2.R|}plot2.R, etc.) that constructs the corresponding plot, i.e. code in \color{red}{\verb|plot1.R|}plot1.R constructs the \color{red}{\verb|plot1.png|}plot1.png plot. Your code file should include code for reading the data so that the plot can be fully reproduced. You must also include the code that creates the PNG file.
-# Add the PNG file and R code file to the top-level folder of your git repository (no need for separate sub-folders)
-
-# Coursera EDA 
+# Coursera EDA Plot1
 # download file and unzip
-download <- download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile = "power.zip")
-unzip("power.zip")
-unlink("power.zip")
+downloadFile <- download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                              destfile = "household_power.zip")
+unzip("household_power.zip")
+# remove zip file as no longer needed
+unlink("household_power.zip")
 
+# read text file and convert date/time fields for analysis
 power <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?")
+power$DateTime <- strptime(paste(power$Date, power$Time), "%d/%m/%Y %H:%M:%S")
 power$Date <- as.Date(power$Date, "%d/%m/%Y")
-power$dt <- strptime(paste(power$Date, power$Time), "%d/%m/%Y %H:%M:%S")
-power_sub <- subset(power, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+
+# keep only dates requested for process
+power <- subset(power, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+
+# Save plot as PNG
+png(file="plot1.png", width=480, height=480)
+hist(power$Global_active_power, 
+     col = "red", 
+     xlab = "Global Active Power (kilowatts)", 
+     ylab = "Frequency", main = "Global Active Power", 
+     breaks = 13, 
+     ylim = c(0,1200), 
+     xlim = c(0, 6)
+)
+dev.off()
